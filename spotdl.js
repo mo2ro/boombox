b0f4200293110ba2c1fd8e8492fd03f038e56ff2
CodeSandbox <support@codesandbox.io>
2025-03-01 19:51:28 +0000

    Sandbox snapshot commit

---
+++ spotdl.js
@@ -0,0 +1,9 @@
+const SpottyDL = require("spottydl");
+(async () => {
+  await SpottyDL.getTrack(
+    "https://open.spotify.com/track/4cOdK2wGLETKBW3PvgPWqT"
+  ).then(async (results) => {
+    let track = await SpottyDL.downloadTrack(results); // Second parameter is optional...
+    console.log(track);
+  });
+})();
